7df350e68d1c4fe94b2b79376a0799b3fe80f550
Sameer Sinha <sameer.882000@gmail.com>
2024-10-05 20:46:57 +0530

    second commit💀

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-angular';
+    closeMenu() {
+    const navbarToggler = document.querySelector('.navbar-toggler') as HTMLElement;
+    const navbarCollapse = document.getElementById('navbarNav');
+
+    // Manually collapse the navbar
+    if (navbarCollapse?.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+      navbarToggler.setAttribute('aria-expanded', 'false');
+    }
+  }
 }
