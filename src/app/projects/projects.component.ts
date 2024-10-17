00cbc60df4c16df9e0868c2a08568aff13b1d3fd
Sameer Sinha <sameer.882000@gmail.com>
2024-10-17 23:56:52 +0530

    project update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
@@ -17,22 +17,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     ])
   ]
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent  {
   projects = [
     {
       title: 'Notes App',
       description: 'A responsive web application for managing notes, built with the MEAN stack (MongoDB, Express.js, Angular, Node.js), featuring CRUD operations, timestamps, and an intuitive user interface.',
       link: 'https://notes-app-tau-five-81.vercel.app/'
     },
-    // {
-    //   title: 'E-commerce Website',
-    //   description: 'An online store with shopping cart and payment gateway integration.',
-    //   link: 'https://your-link-to-ecommerce.com'
-    // }
+    {
+      title: 'BlogKaro.com',
+      description: 'The Blog Application is a full-stack web platform built with EJS for the frontend and Node.js, Express, and MongoDB for the backend, enabling users to create and comment on blog posts with secure authentication.',
+      link: 'https://blog-app-production-55f0.up.railway.app/'
+    }
   ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 }
