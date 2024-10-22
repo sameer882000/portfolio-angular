60264b940b748d36c615002a24c2d3955cf1a390
Sameer Sinha <sameer.882000@gmail.com>
2024-10-22 21:25:56 +0530

    Update hero section styling and content

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -19,6 +19,11 @@ import { trigger, style, animate, transition } from '@angular/animations';
 })
 export class ProjectsComponent  {
   projects = [
+      {
+      title: 'Weather App',
+     description: 'A user-friendly weather application built with Angular and TypeScript that provides real-time weather updates by fetching data from the OpenWeatherMap API. Users can easily check current weather conditions and view a detailed hourly forecast for any city across the globe.',
+      link: 'https://weather-app-cyan-mu-31.vercel.app/'
+    },
     {
       title: 'Notes App',
       description: 'A responsive web application for managing notes, built with the MEAN stack (MongoDB, Express.js, Angular, Node.js), featuring CRUD operations, timestamps, and an intuitive user interface.',
@@ -28,7 +33,8 @@ export class ProjectsComponent  {
       title: 'BlogKaro.com',
       description: 'The Blog Application is a full-stack web platform built with EJS for the frontend and Node.js, Express, and MongoDB for the backend, enabling users to create and comment on blog posts with secure authentication.',
       link: 'https://blog-app-production-55f0.up.railway.app/'
-    }
+    },
+
   ];
 
   constructor() { }
